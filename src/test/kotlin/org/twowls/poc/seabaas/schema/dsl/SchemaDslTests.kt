48f065adc17a0167bae0847b606e5536edefd6d9
Dmitry Chubarov <dime@twowls.org>
2024-01-29 23:16:15 +0400

    Improve tests.

--- src/test/kotlin/org/twowls/poc/seabaas/schema/dsl/SchemaDslTests.kt
+++ src/test/kotlin/org/twowls/poc/seabaas/schema/dsl/SchemaDslTests.kt
@@ -1,24 +1,27 @@
 package org.twowls.poc.seabaas.schema.dsl
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldMatchInOrder
 import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.shouldBe
 
-class SchemaDslTests : StringSpec({
-    "should build a registry with simple object consisting of two fields" {
+class SchemaDslTests : FunSpec({
+    test("should build a registry with simple object consisting of two fields") {
         val res = createSchemaRegistry {
             schema("test") {
                 field("a")
                 field("b")
             }
         }
-        res["test"].shouldNotBeEmpty()
-        res["test"]?.shouldMatchInOrder({ it.name shouldBe "a" }, { it.name shouldBe "b" })
+        res["test"].shouldNotBeEmpty().shouldMatchInOrder({ it.name shouldBe "a" }, { it.name shouldBe "b" })
     }
 
-    "!prints out test schemas" {
-        println(TestSchemas.blog.toStringHierarchy(open = " {{", close = "}}"))
-        println(TestSchemas.ticket.toStringHierarchy(open = " {{", close = "}}"))
+    xcontext("usage examples") {
+        test("prints out test schemas") {
+            println(TestSchemas.blog.toStringHierarchy(open = " {{", close = "}}"))
+            println(TestSchemas.ticket.toStringHierarchy(open = " {{", close = "}}"))
+        }
     }
+
+    beforeTest { }
 })
